eefa244961639c186706c1448f677a6cf56c9c54
Friedrich Kerlach <friedrich.kerlach@de.bosch.com>
2018-08-23 19:57:42 +0200

    first commit

---
+++ 2-notesNode/app.js
@@ -0,0 +1,25 @@
+console.log("application start");
+
+const fs = require("fs");
+const os = require("os");
+const notes = require("./notes");
+const _ = require("lodash");
+
+var user = os.userInfo();
+var res = notes.addNote();
+
+console.log(`some new note from notes.js ${res}`);
+console.log(_.isString("blabla"));
+
+var filteredArray = _.uniq(["andreww", 3, "andreww", 4, 3, 2, 1, 1, 1]);
+console.log(filteredArray);
+
+fs.appendFileSync(
+    "greetings.txt",
+    `hello ${user.username}!! (<- ES6 syntax feature) - your age is ${
+        notes.age
+    }\n`
+);
+console.log(
+    "Hello Master " + user.username + ", the file was successfully created."
+);
---
+++ 2-notesNode/notes.js
@@ -0,0 +1,10 @@
+console.log("hello notes.js");
+
+// console.log(module);
+
+module.exports.age = "drölf";
+
+module.exports.addNote = () => {
+    console.log("addNote");
+    return "NewNote";
+};
